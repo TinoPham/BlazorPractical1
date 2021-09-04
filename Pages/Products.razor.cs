5d6ccbbe7b7192a1d2d857f8f71d929373e114a7
Tino Pham <tino@hmsfirst.com>
2021-09-04 16:43:44 +0700

    apply searching data

--- Pages/Products.razor.cs
+++ Pages/Products.razor.cs
@@ -36,5 +36,13 @@ namespace BlazorProducts.Client.Pages
             ProductList = pagingResponse.Items;
             MetaData = pagingResponse.MetaData;
         }
+
+        private async Task SearchChanged(string searchTerm)
+        {
+            Console.WriteLine(searchTerm);
+            _productParameters.PageNumber = 1;
+            _productParameters.SearchTerm = searchTerm;
+            await GetProductsAsync();
+        }
     }
 }
